5af2e40f9d8210fa0fcb39820fcfbb8271f00fd3
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2023-01-23 18:10:11 -0700

    Added boilerplate code

--- project1.py
+++ project1.py
@@ -1,3 +1,12 @@
-import pymd
+import http.client as httplib
+from urllib.parse import urlparse, quote
+import sys, re
+from pymd5 import *
+url = sys.argv[1]
 
-print("test")
+# Your code to modify the URL goes here!
+
+parsedUrl = urlparse(url)
+conn = httplib.HTTPConnection(parsedUrl.hostname,parsedUrl.port)
+conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
+print(conn.getresponse().read())
