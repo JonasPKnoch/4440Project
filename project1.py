58470931d3328403d8b90f461c65b94264502041
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2023-01-23 19:03:29 -0700

    messed around

--- project1.py
+++ project1.py
@@ -1,12 +1,25 @@
 import http.client as httplib
 from urllib.parse import urlparse, quote
 import sys, re
-from pymd5 import *
-url = sys.argv[1]
+from pymd import *
+#url = sys.argv[1]
+url = "http://cs4440.eng.utah.edu/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp"
+len_m = 8 + len(quote("user=admin&command1=ListFiles&command2=NoOp"))
 
-# Your code to modify the URL goes here!
+x = quote("&user=admin&command1=ListFiles&command2=NoOp")
+print(quote("&command3=UnlockAllSafes"))
 
-parsedUrl = urlparse(url)
+bits = (len_m + len(padding(len_m*8)))*8
+print(bits)
+h = md5(state="402a574d265dc212ee64970f159575d0", count=bits)
+h.update(x)
+new_hash = h.hexdigest()
+print(new_hash)
+
+modified_url = "http://cs4440.eng.utah.edu/project1/api?token=" + new_hash + "&user=admin&command1=ListFiles&command2=NoOp&command3=UnlockAllSafes"
+print(modified_url)
+
+parsedUrl = urlparse(modified_url)
 conn = httplib.HTTPConnection(parsedUrl.hostname,parsedUrl.port)
 conn.request("GET", parsedUrl.path + "?" + parsedUrl.query)
 print(conn.getresponse().read())
--- pymd.py
+++ pymd.py
@@ -347,14 +347,19 @@ def test(input=""):
     print(repr(hashlib.md5(input.encode('utf-8')).hexdigest()))
 
 if __name__=="__main__":
+    x = "Suffix"
+
     m = "Use HMAC, not hashes"
-    x = "Good advice"
-    h = md5()
-    h.update(m)
-    print(h.hexdigest())
     h2 = md5()
     h2.update(m + padding(len(m)*8) + x)
     print(h2.hexdigest())
+
+    bits = (len(m) + len(padding(len(m)*8)))*8
+    print(bits)
+    
+    h = md5()
+    #h.update(m)
+    print(h.hexdigest())
     h = md5(state="3ecc68efa1871751ea9b0b1a5b25004d", count=512)
     h.update(x)
     print(h.hexdigest())
