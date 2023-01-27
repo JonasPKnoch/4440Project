ec20ca2837e6fd8179c173fa3546ce3998ec1788
JonasPKnochelmann <jonas.p.knochelmann@gmail.com>
2023-01-26 23:03:26 -0700

    Got 1.2 working

--- project1.py
+++ project1.py
@@ -2,22 +2,22 @@ import http.client as httplib
 from urllib.parse import urlparse, quote
 import sys, re
 from pymd import *
-#url = sys.argv[1]
-url = "http://cs4440.eng.utah.edu/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp"
-len_m = 8 + len(quote("user=admin&command1=ListFiles&command2=NoOp"))
+url = sys.argv[1]
+#url = "http://cs4440.eng.utah.edu/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp"
 
-x = quote("&user=admin&command1=ListFiles&command2=NoOp")
-print(quote("&command3=UnlockAllSafes"))
+url_begin = url[ : url.index("token=") + 6]
+url_token = url[url.index("token=") + 6 : url.index("&user=")]
+url_end = url[url.index("user="): ]
 
-bits = (len_m + len(padding(len_m*8)))*8
-print(bits)
-h = md5(state="402a574d265dc212ee64970f159575d0", count=bits)
-h.update(x)
-new_hash = h.hexdigest()
-print(new_hash)
+m_len = 8 + len(url_end)
+bits = (m_len + len(padding(m_len*8)))*8
 
-modified_url = "http://cs4440.eng.utah.edu/project1/api?token=" + new_hash + "&user=admin&command1=ListFiles&command2=NoOp&command3=UnlockAllSafes"
-print(modified_url)
+h = md5(state=url_token, count=bits)
+url_suffix = "&command3=UnlockAllSafes"
+h.update(url_suffix)
+
+new_token = h.hexdigest()
+modified_url = url_begin + new_token + "&" + url_end + quote(padding(m_len*8)) + url_suffix
 
 parsedUrl = urlparse(modified_url)
 conn = httplib.HTTPConnection(parsedUrl.hostname,parsedUrl.port)
